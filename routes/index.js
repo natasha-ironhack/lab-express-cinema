01f6c8ca839876fa711bc556a006859a27b58b79
Natasha Smith <pisceanchic@gmail.com>
2021-08-25 12:29:12 +0200

    almost done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  DeviceMotionEvent.find()
+    .then((returnedMovies) => {
+      console.log(returnedMovies);
+      res.render("movies", { returnedMovies });
+      //res.send(returnedMovies);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movie.find({ _id: req.params.id })
+    .then((returnedMovie) => {
+      res.render("movies-details", { returnedMovie });
+      // res.send(returnedMovies);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
